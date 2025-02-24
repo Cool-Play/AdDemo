ef9ce76baf03cbf58d532b9ce897cbb4d5a42163
lixiaohong <617644384@qq.com>
2025-02-24 16:51:34 +0800

    coolplay

--- app/src/main/java/cn/coolplay/mundonoticias/MainActivity.kt
+++ app/src/main/java/cn/coolplay/mundonoticias/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import cn.coolplay.sdk.AdImaType
 import cn.coolplay.sdk.CoolPlaySdk
 import cn.coolplay.sdk.loader.vast.AdVastRequestBuilder
+import cn.coolplay.sdk.ui.vast.video.VideoPlayerController
 import cn.coolplay.sdk.utils.print
+
 
 //import com.zeasn.ad.connector.ZeasnAd
 //import com.zeasn.ad.connector.bean.ZeasnAdInfo
@@ -22,13 +24,40 @@ class MainActivity : AppCompatActivity() {
     //    var bannerAdPC: PluginPlayerControl? = null
     private var adContainer1: FrameLayout? = null
     var adContainer2: FrameLayout? = null
-//    val mHandle = Handler(Looper.getMainLooper())
+    var adContainer: FrameLayout? = null
+
+    //    val mHandle = Handler(Looper.getMainLooper())
 //    private var bannerAd: ZeasnAd? = null
 //    private val param by lazy {
 //        val param: MutableMap<String, Any> = HashMap()
 //        param[ZeasnAdKey.unitViewId] = "stb_banner_view"
 //        param
 //    }
+    var adIndex = 0
+    private val adRequestBuilder = AdVastRequestBuilder(
+        this,
+        testVastLink = "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_preroll_skippable&sz=640x480&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=",
+        adImaType = AdImaType.SECTION,
+    ) { adRequestBuilder, channel: String ->
+        "channel:$channel".print("dddddd")
+        if (channel == "google") {
+            //这里可能会有多次回掉自己处理
+            adContainer2?.let {
+                adRequestBuilder.start(
+                    it,
+                    contentVideoUrl = "https://storage.googleapis.com/gvabox/media/samples/stock.mp4",
+                )
+            }
+        } else {
+            adContainer?.let {
+                adRequestBuilder.start(
+                    it,
+                    contentVideoUrl = "https://storage.googleapis.com/gvabox/media/samples/stock.mp4",
+                )
+            }
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,25 +75,48 @@ class MainActivity : AppCompatActivity() {
             view.setBackgroundColor(if (b) Color.BLUE else Color.RED)
             view.scaleY = if (b) 1.2f else 1f
         }
-        findViewById<FrameLayout>(R.id.adContainer).setOnFocusChangeListener { view, b ->
+        adContainer = findViewById(R.id.adContainer)
+        adContainer?.setOnFocusChangeListener { view, b ->
             view.setBackgroundColor(if (b) Color.BLUE else Color.RED)
             view.scaleY = if (b) 1.2f else 1f
         }
 
 //        loadAdBanner()
 
-        CoolPlaySdk.preloadAd(
-            adRequestBuilder = AdVastRequestBuilder(
-                this,
-                adImaType = AdImaType.SECTION,
-                contentVideoUrl = ""
-            )
-        ) {
-            "成功加载".print()
-            CoolPlaySdk.loadVastAd(adContainer1!!, adImaType = AdImaType.SECTION)
-        }
+        CoolPlaySdk.loadAd(adRequestBuilder)
+//        val controller =
+//            VideoPlayerController(
+//                adRequestBuilder,
+//                "google",
+//                0L,
+//                4000L
+//            )
+//        controller.setCurrentAdTagUrl(adRequestBuilder.testVastLink!!)
+//        controller.requestAndPlayAds()
+//
+//        val controller1 =
+//            VideoPlayerController(
+//                adRequestBuilder,
+//                "miji",
+//                0L,
+//                4000L
+//            )
+//        controller1.setCurrentAdTagUrl(adRequestBuilder.testVastLink!!)
+//        controller1.requestAndPlayAds()
 
+//        val tvSdkView3 = VideoPlayerWithAdPlayback(this)
+//        tvSdkView3.startAd(
+//            contentUrl = "https://storage.googleapis.com/gvabox/media/samples/stock.mp4",
+//            tagString = adRequestBuilder.testVastLink!!
+//        )
+//        tvSdkView3.aaaa = {
+//            "zoulezhelu".print()
+//            adContainer?.addView(tvSdkView3)
+//            tvSdkView3.start {}
+//
+//        }
     }
+
 
 //    val timeRun = Runnable {
 //        bannerAd?.loadAd(param)
